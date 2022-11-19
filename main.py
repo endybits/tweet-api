70e5ac549176231c477429fce07f0f4c30170c7c
endybits <endyb.urbanista@gmail.com>
2022-11-19 17:15:51 -0500

    Users model

--- main.py
+++ main.py
@@ -1,11 +1,52 @@
+from uuid import UUID
+from typing import Optional
+from datetime import date
+# Pydantic
+from pydantic import BaseModel
+from pydantic import Field
+from pydantic import EmailStr
+
+# FastAPI
 from fastapi import FastAPI
 from fastapi import status
 
 app = FastAPI()
 
+# Models
+class UserBase(BaseModel):
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(...)
+
+class UserLogin(UserBase):
+    password: str = Field(
+        ...,
+        min_length=8,
+        max_length=64
+    )
+
+class User(UserBase):
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    birth_day: Optional[date] = Field(default=None)
+
+
+class Tweet(BaseModel):
+    pass
+
+
+# Endpoints
 @app.get(
     path='/',
-    status_code=status.HTTP_200_OK
+    status_code=status.HTTP_200_OK,
+    tags='Home'
 )
 async def home():
     return {
