41c5f623774c33b648e6c9318ba352ddb7cc4354
endybits <endyb.urbanista@gmail.com>
2022-11-19 18:25:00 -0500

    Path operations structure tweets

--- main.py
+++ main.py
@@ -52,10 +52,16 @@ class Tweet(BaseModel):
     owner: User = Field(...)
 
 # Path Operations
+
+## Tweets
+
+### Show all tweets
 @app.get(
     path='/',
+    response_model=List[Tweet],
     status_code=status.HTTP_200_OK,
-    tags='Home'
+    summary='Show all tweets',
+    tags=['Tweets']
 )
 async def home():
     return {
@@ -64,7 +70,54 @@ async def home():
         }
     }
 
+### Create a tweet
+@app.post(
+    path='/post',
+    response_model=Tweet,
+    status_code=status.HTTP_201_CREATED,
+    summary='Post a Tweet',
+    tags=['Tweets']
+)
+async def post_tweet():
+    pass
+
+### Show a tweet
+@app.get(
+    path='/tweets/{tweet_id}',
+    response_model=Tweet,
+    status_code=status.HTTP_200_OK,
+    summary='Show a tweet',
+    tags=['Tweets']
+)
+async def show_a_tweet():
+    pass
+
+### Update a tweet
+@app.put(
+    path='/tweets/{tweet_id}/update',
+    response_model=Tweet,
+    status_code=status.HTTP_200_OK,
+    summary='Update a tweet',
+    tags=['Tweets']
+)
+async def update_a_tweet():
+    pass
+
+### Delete a tweet
+@app.delete(
+    path='/tweets/{tweet_id}/delete',
+    response_model=Tweet,
+    status_code=status.HTTP_200_OK,
+    summary='Delete a tweet',
+    tags=['Tweets']
+)
+async def delete_a_tweet():
+    pass
+
+
 ## Users
+
+### Registe a user
 @app.post(
     path='/users',
     response_model=User,
@@ -75,7 +128,7 @@ async def home():
 async def signup():
     pass
 
-
+### Login a user
 @app.post(
     path='/login',
     response_model=User,
@@ -86,7 +139,7 @@ async def signup():
 async def login():
     pass
 
-
+### Show all users
 @app.get(
     path='/users',
     response_model=List[User],
@@ -97,7 +150,7 @@ async def login():
 async def show_all_users():
     pass
 
-
+### Show user detail
 @app.get(
     path='/users/{user_id}',
     response_model=User,
@@ -108,7 +161,7 @@ async def show_all_users():
 async def show_user_detail():
     pass
 
-
+### Update a user
 @app.put(
     path='/users/{user_id}/update',
     response_model=User,
@@ -119,7 +172,7 @@ async def show_user_detail():
 async def update_an_user():
     pass
 
-
+### Delete a user
 @app.delete(
     path='/users/{user_id}/delete',
     response_model=User,
@@ -129,7 +182,3 @@ async def update_an_user():
 )
 async def delete_an_user():
     pass
-
-
-
-## Tweets
